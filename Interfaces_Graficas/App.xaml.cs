0f027f1e859be20043e8ffef2347f9e27db0c055
Leonel Cuenca <leo.cuenca.hw@gmail.com>
2023-12-28 11:34:16 -0300

    Interfaces Graficas -> Comienzo

---
+++ Interfaces_Graficas/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Interfaces_Graficas
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
